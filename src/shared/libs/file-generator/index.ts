ed60301e123a59d1ee0da1fbd846e7d366f50cc6
Daniil Aliev <de.aliev@yandex.ru>
2024-09-19 15:16:14 +0300

    change generated file from object to array

--- src/shared/libs/file-generator/index.ts
+++ src/shared/libs/file-generator/index.ts
@@ -13,28 +13,26 @@ export class TSVFileGenerator {
   public generate(): string {
     const randomCity = generateRandomItem(this.res.city) as City;
 
-    const generated = {
-      name: generateRandomItem(this.res.name),
-      description: generateRandomItem(this.res.description),
-      publicationDate: generateRandomItem(this.res.publicationDate),
-      city: randomCity.name,
-      previewImage: generateRandomItem(this.res.previewImage),
-      photos: this.joinArr(generateRandomArray(this.res.photos, 6)),
-      isPremium: generateRandomItem(this.res.isPremium),
-      isFavorite: generateRandomItem(this.res.isFavorite),
-      rating: generateRandomNumber(1, 5),
-      housingType: generateRandomItem(this.res.housingType),
-      roomCount: generateRandomNumber(1, 8),
-      guestsCount: generateRandomNumber(1, 10),
-      rentalPrice: generateRandomNumber(100, 100000),
-      amenities: this.joinArr(generateRandomArray(this.res.amenities, 3)),
-      authorPath: generateRandomItem(this.res.authorPath),
-      commentCount: generateRandomNumber(1, 10),
-      coordinates: this.joinArr([randomCity.latitude, randomCity.longitude])
-    };
+    const generated = [
+      generateRandomItem(this.res.name),
+      generateRandomItem(this.res.description),
+      generateRandomItem(this.res.publicationDate),
+      randomCity.name,
+      generateRandomItem(this.res.previewImage),
+      this.joinArr(generateRandomArray(this.res.photos, 6)),
+      generateRandomItem(this.res.isPremium),
+      generateRandomItem(this.res.isFavorite),
+      generateRandomNumber(1, 5),
+      generateRandomItem(this.res.housingType),
+      generateRandomNumber(1, 8),
+      generateRandomNumber(1, 10),
+      generateRandomNumber(100, 100000),
+      this.joinArr(generateRandomArray(this.res.amenities, 3)),
+      generateRandomItem(this.res.authorPath),
+      generateRandomNumber(1, 10),
+      this.joinArr([randomCity.latitude, randomCity.longitude])
+    ];
 
-    const values = Object.values(generated);
-
-    return values.join('\t');
+    return generated.join('\t');
   }
 }
